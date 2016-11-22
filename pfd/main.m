a18c272ccbd06f3f2fc884da4e0d356dd20fd19f
chikami <chikami>
2016-11-22 14:50:15 +0900

    Initial Commit

---
+++ pfd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pfd
+//
+//  Created by chikami on 2016/11/22.
+//  Copyright © 2016年 ex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
